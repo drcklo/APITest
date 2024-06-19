4ea2448ef9fc3027b398d482ef7736b4b1f1bf8e
drcklo <luis_paulino5@ucne.edu.do>
2024-06-19 14:52:12 -0400

    agregando la pantalla a MainActivity

--- app/src/main/java/com/ucne/apitest/MainActivity.kt
+++ app/src/main/java/com/ucne/apitest/MainActivity.kt
@@ -11,37 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ucne.apitest.presentation.games.GamesScreen
 import com.ucne.apitest.ui.theme.APITestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             APITestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GamesScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    APITestTheme {
-        Greeting("Android")
-    }
-}
